3f9e80f9042d7d5b6bb1d4d88ddc5422cd08e94e
bkblanton <brycekblanton@gmail.com>
2017-04-12 03:36:25 -0700

    Bootstrap

--- app/forms.py
+++ app/forms.py
@@ -1,19 +1,23 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
-from wtforms import IntegerField
-from wtforms.validators import DataRequired
+from wtforms import IntegerField, TextAreaField
+from wtforms.validators import DataRequired, Length
 
 
 class TextFileForm(FlaskForm):
-    file = FileField(label='File', validators=[
-        FileRequired(message='You must include a text file.'),
-        FileAllowed(['txt'], message='You may only upload .txt files.')
+    # file = FileField(label='File', validators=[
+    #     FileRequired(message='You must include a text file.'),
+    #     FileAllowed(['txt'], message='You may only upload .txt files.')
+    # ])
+    text = TextAreaField(label='Text', validators=[
+        DataRequired(message='You must include some text to analyze.'),
+        Length(min=1, max=5000000, message='Your text size must be less than 5 megabytes.')
     ])
     sentences = IntegerField(label='Sentences', default=1, validators=[
-        DataRequired(message='You must specify at least 1 sentence.'),
+        DataRequired(message='You must specify at least 1 sentence per output.'),
         lambda form, field: field.data > 0
     ])
-    number = IntegerField(label='Number', default=1, validators=[
-        DataRequired(),
+    number = IntegerField(label='How many?', default=1, validators=[
+        DataRequired(message='You must specify at least 1 output to generate.'),
         lambda form, field: field.data > 0
     ])
--- app/pages.py
+++ app/pages.py
@@ -1,16 +1,21 @@
 from flask import Blueprint, render_template
 from app.forms import TextFileForm
 from app.markov import Markov
+import re
 
 pages = Blueprint('pages', __name__)
+
+line_re = re.compile(r'(.*)(?:\r?\n|$)')
 
 
 @pages.route('/', methods=('GET', 'POST'))
 def home():
     form = TextFileForm()
     if form.validate_on_submit():
-        file = form.file.data
-        markov = Markov.from_lines(line.decode('utf-8', errors='ignore') for line in file.stream.readlines())
+        # file = form.file.data
+        text = form.text.data
+        # markov = Markov.from_lines(match.group(1) for match in line_re.finditer(text))
+        markov = Markov.from_string(text)
         outputs = []
         for _ in range(form.number.data):
             output = ' '.join(markov.generate(n=form.sentences.data))
