15f35c3e20031c8bb5d15ca8bac80aa37fe5f80e
bkblanton <brycekblanton@gmail.com>
2017-04-12 18:47:49 -0700

    Refined style

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_pyfile('config.cfg', silent=True)
+    app.config.from_object('config.production')
 
     app.register_blueprint(pages)
 
